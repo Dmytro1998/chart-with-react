620cfc03db9c1543dce113c11b35f181b227baaf
Dmytro <evdokimov041998@gmail.com>
2021-10-08 21:18:04 +0300

    50%of application

---
+++ my-app/src/components/todo.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+ const Todo =()=>{
+    return (
+        <div className="todo-container">
+      <ul className="todo-list"></ul>
+    </div>
+
+
+    );
+
+}
+export default Todo
