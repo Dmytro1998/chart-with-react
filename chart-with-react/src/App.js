a4a60d5028a0e4f5c5084ca8bb88b8a49f338af9
Dmytro <evdokimov041998@gmail.com>
2021-10-19 14:21:17 +0300

    done

---
+++ chart-with-react/src/App.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import './App.css';
+import  {getData} from '../src/components/datageneration'
+import BarChart from './components/Barchart';
+import LineChart from './components/lineChart';
+import DoughnutChart from './components/DoughnutChart';
+
+
+class App extends React.Component {
+    constructor(props) {
+      super(props);
+  
+      this.state = {
+        data: getData()
+      };
+    }
+  
+    componentDidMount() {
+      window.setInterval(() => {
+        this.setState({
+          data: getData()
+        })
+      }, 5000)
+    }
+  
+    render() {
+      return (
+        <div className="App">
+          <div className="main chart-wrapper">
+            <LineChart
+              data={this.state.data[0].data}
+              title={this.state.data[0].title}
+              color="#3E517A"
+            />
+          </div>
+          <div className="sub chart-wrapper">
+            <BarChart
+              data={this.state.data[1].data}
+              title={this.state.data[1].title}
+              color="#70CAD1"
+            />
+          </div>
+          <div className="sub chart-wrapper">
+            <BarChart
+              data={this.state.data[2].data}
+              title={this.state.data[2].title}
+              color="#B08EA2"
+            />
+          </div>
+          <div className="sub chart-wrapper">
+            <DoughnutChart
+              data={this.state.data[3].data}
+              title={this.state.data[3].title}
+              colors={['#a8e0ff', '#8ee3f5', '#70cad1', '#3e517a', '#b08ea2', '#BBB6DF']}
+            />
+          </div>
+        </div>
+      );
+    }
+  }
+
+  export default App
